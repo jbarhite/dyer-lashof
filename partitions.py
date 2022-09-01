31222f084f60f4c85ac449144bbd7b78608623a0
Justin Barhite <justin.a.barhite@gmail.com>
2022-09-01 10:29:59 -0400

    partitionsByPowersOf2 optionally returns a vector of exponents instead of a list of powers of 2

--- partitions.py
+++ partitions.py
@@ -1,10 +1,11 @@
 class partitionsByPowersOf2:
 
-	def __init__(self, n):
+	def __init__(self, n, outputExponentVector=False):
 		if not isinstance(n, int) or n < 1:
 			raise ValueError("Input must be a positive integer.")
 		self.n = n
 		self.p = []
+		self.oEV = outputExponentVector
 
 
 	def __iter__(self):
@@ -13,21 +14,21 @@ class partitionsByPowersOf2:
 
 	def __next__(self):
 		if len(self.p) == 0:
-			s = "{0:b}".format(self.n)
-			self.p = [2**(len(s) - i - 1) for i in range(len(s)) if s[i] == "1"]
-			return self.p
+			self.p = [int(x) for x in "{0:b}".format(self.n)[::-1]]
+			return self.p[:] if self.oEV else [2**i for i in range(len(self.p)) if self.p[i] == 1][::-1]
 
-		i = len(self.p) - 1
-		while i >= 0 and self.p[i] == 1: i -= 1
-		if i == -1:
+		if self.p[0] == self.n:
 			raise StopIteration
 
-		self.p = self.p[:i] + [self.p[i] // 2]
-		maxpart = self.p[i]
-		remaining = self.n - sum(self.p)
-		while remaining > 0:
-			while maxpart > remaining:
-				maxpart //= 2
-			self.p.append(maxpart)
-			remaining -= maxpart
-		return self.p
+		l = len(self.p)
+		for i in range(1, l):
+			if self.p[i] != 0:
+				self.p = [0 for j in range(i)] + ([self.p[i] - 1] + self.p[i+1:] if i < l - 1 or self.p[i] > 0 else [])
+				r = self.n - sum([2**j * self.p[j] for j in range(i, len(self.p))])
+				for j in range(i - 1, -1, -1):
+					while r >= 2**j:
+						self.p[j] += 1
+						r -= 2**j
+
+				if self.oEV: return self.p[:]
+				return sum([[2**i for j in range(self.p[i])] for i in range(len(self.p))], [])[::-1]
