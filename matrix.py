e6bf86a25222c99fab1b883f023d01050a8df350
Justin Barhite <justin.a.barhite@gmail.com>
2022-09-03 15:42:04 -0400

    Allowing matrices to take values in Z/nZ

--- matrix.py
+++ matrix.py
@@ -1,10 +1,11 @@
 import copy
 
 class Matrix:
-	def __init__(self, v):
+	def __init__(self, v, mod=False):
 		self.m = len(v)
 		self.n = len(v[0])
 		self.v = copy.deepcopy(v) # values
+		self.mod = mod
 
 
 	def inv_mod2(self):
@@ -27,26 +28,44 @@ class Matrix:
 				if A[r][i] == 1:
 					for c in range(2*n): A[r][c] = (A[r][c] + A[i][c]) % 2
 
-		return Matrix([[A[r][n + c] for c in range(n)] for r in range(n)])
+		return Matrix([[A[r][n + c] for c in range(n)] for r in range(n)], mod=2)
 
 
 	def det(self):
 		if self.m != self.n:
 			raise ValueError("Cannot take the determinant of a non-square matrix.")
+
 		if self.m == 1: return self.v[0][0]
-		return sum([self.v[0][i] * ((-1)**i) * Matrix([[self.v[j][k] for k in list(range(0, i)) + list(range(i+1, self.m))] for j in range(1, self.m)]).det() for i in range(self.m)])
+		ans = sum([self.v[0][i] * ((-1)**i) * Matrix([[self.v[j][k] for k in list(range(0, i)) + list(range(i+1, self.m))] for j in range(1, self.m)]).det() for i in range(self.m)])
+		return ans if self.mod == False else ans % self.mod
 
 
 	def __add__(self, other):
 		if self.m != other.m or self.n != other.n:
 			raise ValueError("Cannot add a {}x{} matrix and a {}x{} matrix.".format(self.m, self.n, other.m, other.n))
-		return Matrix([[self.v[r][c] + other.v[r][c] for c in range(self.n)] for r in range(self.m)])
+		if self.mod != other.mod:
+			raise ValueError("Cannot add matrices with entries in different rings.")
+
+		ans = Matrix([[self.v[r][c] + other.v[r][c] for c in range(self.n)] for r in range(self.m)])
+		if self.mod != False:
+			for i in range(ans.m):
+				for j in range(ans.n):
+					ans.v[i][j] %= self.mod
+		return ans
 
 
 	def __mul__(self, other):
 		if self.n != other.m:
 			raise ValueError("Cannot multiply a {}x{} matrix and a {}x{} matrix.".format(self.m, self.n, other.m, other.n))
-		return Matrix([[sum([self.v[r][i] * other.v[i][c] for i in range(self.n)]) for c in range(other.n)] for r in range(self.m)])
+		if self.mod != other.mod:
+			raise ValueError("Cannot add matrices with entries in different rings.")
+
+		ans = Matrix([[sum([self.v[r][i] * other.v[i][c] for i in range(self.n)]) for c in range(other.n)] for r in range(self.m)])
+		if self.mod != False:
+			for i in range(ans.m):
+				for j in range(ans.n):
+					ans.v[i][j] %= self.mod
+		return ans
 
 
 	def __str__(self):
